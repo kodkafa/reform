9a18250c70de018d95809acfc5d79a406543f8e7
Cagatay Kaydir <cagataykaydir@gmail.com>
2023-03-10 22:34:24 +0300

    changes this far

--- src/stories/InputGroup.stories.tsx
+++ src/stories/InputGroup.stories.tsx
@@ -96,6 +96,99 @@ export const MultipleInputs = {
   },
 };
 
+export const LeadingIcon = {
+  render: (args: Props) => (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup {...args}>
+          <svg
+            className='h-4 w-4 text-gray-400 ml-2'
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            fill='currentColor'
+            viewBox='0 0 16 16'
+          >
+            <path d='M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z' />
+          </svg>
+
+          <Input className='h-10' name='input' placeholder='Leading' />
+        </InputGroup>
+        <Submit>Submit</Submit>
+      </Form>
+    </div>
+  ),
+  args: {
+    name: 'group',
+  },
+};
+
+export const TrailingIcon = {
+  render: (args: Props) => (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup {...args}>
+          <Input className='h-10' name='input' placeholder='Trailing' />
+          <svg
+            className='h-4 w-4 text-gray-400 mr-2'
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            fill='currentColor'
+            viewBox='0 0 16 16'
+          >
+            <path d='M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z' />
+            <path d='M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z' />
+          </svg>
+        </InputGroup>
+        <Submit>Submit</Submit>
+      </Form>
+    </div>
+  ),
+  args: {
+    name: 'group',
+  },
+};
+
+export const LeadingAndTrailingIcon = {
+  render: (args: Props) => (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup {...args}>
+          <svg
+            className='h-4 w-4 text-gray-400 ml-2'
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            fill='currentColor'
+            viewBox='0 0 16 16'
+          >
+            <path d='M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z' />
+          </svg>
+
+          <Input className='h-10' name='input' placeholder='Leading And Trailing' />
+
+          <svg
+            className='h-4 w-4 text-gray-400 mr-2'
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            fill='currentColor'
+            viewBox='0 0 16 16'
+          >
+            <path d='M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z' />
+            <path d='M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z' />
+          </svg>
+        </InputGroup>
+        <Submit>Submit</Submit>
+      </Form>
+    </div>
+  ),
+  args: {
+    name: 'group',
+  },
+};
+
 export const Addon = {
   render: (args: Props) => (
     <div>
@@ -134,13 +227,28 @@ export const InlineAddon = {
   },
 };
 
-export const WithInputIcon = {
+export const MultipleAddon = {
   render: (args: Props) => (
     <div>
       <Form onSubmit={handleSubmit}>
         <InputGroup {...args}>
-          <Input className='ml-4' name='input' placeholder='Please write something..' />
-          <p className='text-gray-500 mr-4'>USD</p>
+          <p className='h-10 px-4 border-r border-gray-200 inline-flex items-center w-max rounded-l-md bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:text-gray-400'>
+            $
+          </p>
+          <p className='h-10 px-4 mr-2 border-r border-gray-200 inline-flex items-center w-max rounded-l-md bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:text-gray-400'>
+            0.00
+          </p>
+          <Input className='h-10' name='input-1' placeholder='Please write something..' />
+        </InputGroup>
+
+        <InputGroup {...args}>
+          <Input className='h-10' name='input-2' placeholder='Please write something..' />
+          <p className='h-10 px-4 border-l border-gray-200 inline-flex items-center w-max bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:text-gray-400'>
+            $
+          </p>
+          <p className='h-10 px-4 border-l border-gray-200 inline-flex items-center w-max bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:text-gray-400'>
+            0.00
+          </p>
         </InputGroup>
         <Submit>Submit</Submit>
       </Form>
@@ -149,6 +257,51 @@ export const WithInputIcon = {
   args: {
     name: 'group',
   },
+};
+
+const searchSchema = Yup.object().shape({
+  type: Yup.string().equals(['users', 'groups', 'posts']).required('Required'),
+  q: Yup.string()
+    .trim()
+    .matches(/[A-z 0-9]+/)
+    .required('Required'),
+});
+export const TrailingButtonAddon = {
+  render: (args: Props) => (
+    <div>
+      <Form onSubmit={handleAsyncSubmitWithError} schema={searchSchema}>
+        <InputGroup {...args}>
+          <Select
+            name='type'
+            className='mx-2'
+            options={[
+              { children: 'Users', value: 'users' },
+              { children: 'Groups', value: 'groups' },
+              { children: 'Posts', value: 'posts' },
+            ]}
+            placeholder='search in'
+          />
+          <Input className='!border-l' name='input-1' placeholder='search ...' />
+          <Submit className='!rounded-l-none'>SEARCH</Submit>
+        </InputGroup>
+
+        <InputGroup {...args}>
+          <svg
+            className='h-4 w-4 text-gray-400 ml-2'
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            fill='currentColor'
+            viewBox='0 0 16 16'
+          >
+            <path d='M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z' />
+          </svg>
+
+          <Input className='h-10' name='input-2' placeholder='Leading' />
+        </InputGroup>
+      </Form>
+    </div>
+  ),
 };
 
 export const WithLabel = {
@@ -194,34 +347,4 @@ export const WithError = {
   args: {
     name: 'group',
   },
-};
-
-const searchSchema = Yup.object().shape({
-  type: Yup.string().equals(['users', 'groups', 'posts']).required('Required'),
-  q: Yup.string()
-    .trim()
-    .matches(/[A-z 0-9]+/)
-    .required('Required'),
-});
-export const WithSearchButton = {
-  render: (args: Props) => (
-    <div>
-      <Form onSubmit={handleAsyncSubmitWithError} schema={searchSchema}>
-        <InputGroup {...args}>
-          <Select
-            name='type'
-            options={[
-              { children: 'Users', value: 'users' },
-              { children: 'Groups', value: 'groups' },
-              { children: 'Posts', value: 'posts' },
-            ]}
-            className='mx-2'
-            placeholder='search in'
-          />
-          <Input className='!border-l' name='q' placeholder='search ...' />
-          <Submit className='!rounded-l-none'>SEARCH</Submit>
-        </InputGroup>
-      </Form>
-    </div>
-  ),
 };
